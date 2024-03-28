c46f40b929ac5638382620a8ce07ec73b331acb4
vicentepaiva <vicentepaivadev@gmail.com>
2024-03-27 22:53:50 -0300

    adding advise

--- index.js
+++ index.js
@@ -1,52 +1,45 @@
 const BASE_URL = 'http://universities.hipolabs.com/search';
 
-
 const countryValues = {
     'brazil': 'Brazil',
     'United States': 'United States',
-}
+};
 
 function fetchUniversities(queryParams) {
-    if (queryParams.includes('limit')) 
-        document.getElementById('countrySelect').value = '';
-    
+    const url = queryParams ? `${BASE_URL}${queryParams}` : BASE_URL;
 
-fetch (`${BASE_URL}${queryParams}`) 
-    .then(response => response.json())
-    .then(data => showUniversities(data))
-    .catch(error => console.log(error));
+    if (!queryParams || queryParams.includes('limit')) 
+        document.getElementById('countrySelect').value = '';
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => showUniversities(data))
+        .catch(error => console.log(error));
 }
 
 fetchUniversities('');
 
-
 function showUniversities(universities) {
     const universitiesList = document.getElementById('universitiesList');
     universitiesList.innerHTML = '';
-    universities.forEach(university => {
-        const universityItem = document.createElement('div');
-        universityItem.textContent = university.name + ' - ' + university.country;
-        universitiesList.appendChild(universityItem);
-    })
-    universities.sort((pirmeiraOcorrencia, segundaOcorrencia) => {
-        const nomeA = pirmeiraOcorrencia.name.toUpperCase();
-        const nomeB = segundaOcorrencia.name.toUpperCase();
-        if(nomeA < nomeB) {
-            return -1;
-        }
-        if(nomeA > nomeB) {
-            return 1;
-        }
-        return 0;
-    })
+
+    if (universities.length === 0) {
+        universitiesList.textContent = 'Nenhuma lista de unversidades selecionada.';
+    } else {
+        universities.forEach(university => {
+            const universityItem = document.createElement('div');
+            universityItem.textContent = university.name + ' - ' + university.country;
+            universitiesList.appendChild(universityItem);
+        });
+    }
 }
 
 document.getElementById('countrySelect').addEventListener('change', (event) => {
     const selectedCountry = event.target.value;
-    if(selectedCountry) {
+    if (selectedCountry) {
         fetchUniversities(`?country=${countryValues[selectedCountry]}`);
     } else {
-        fetchUniversities('');
+       
+        showUniversities([]);
     }
 });
-
